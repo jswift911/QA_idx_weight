c0fbe567893bfa7e918ad1ef34e8af1f05fa021f
Alex <jswift888@gmail.com>
2023-02-25 12:27:05 +0300

    bmi service

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BmiService service = new BmiService();
+        int index = service.calculate(1.87, 98);
+        System.out.println(index);
     }
 }
